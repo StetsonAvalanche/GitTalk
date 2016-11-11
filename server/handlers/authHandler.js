01344568a10998bbc91437399f645b7df3bea47c
Chase Starr <chasestarr@gmail.com>
2016-11-10 16:40:28 -0800

    (setup) added boilerplate auth hander and route files

--- server/handlers/authHandler.js
+++ server/handlers/authHandler.js
@@ -1,2 +1,10 @@
 // handles login paths
 // request handlers should have the (req, res) argument signature
+
+function login(req, res) {
+  res.status(200).end('hello world');
+}
+
+module.exports = {
+  login
+};
