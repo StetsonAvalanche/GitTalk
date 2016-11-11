4dc63bdeabd6dead66e72bb7595f34adec523be9
Chase Starr <chasestarr@gmail.com>
2016-11-11 11:26:06 -0800

    (feat) github login w passport, auth/user route for getting info

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const authHandler = require('./../handlers/authHandler.js');
-const passport = require('./../passportConfig.js');
+const authHandler = require('../handlers/authHandler.js');
+const passport = require('../passport/config.js');
 const router = express.Router();
 
 // routes for github authentication
@@ -8,6 +8,8 @@ const router = express.Router();
 // hello world example
 router.get('/github', passport.authenticate('github', { scope: [ 'user:email' ] }));
 
-router.get('/github/callback', passport.authenticate('github', { failureRedirect: '/' }), authHandler.postLogin);
+router.get('/github/callback', passport.authenticate('github', { failureRedirect: '/' }), authHandler.postAuth);
+
+router.get('/user', authHandler.getUser);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
+const path = require('path');
 // import env variables from .env - if not found, fallback to OS env vars
-require('dotenv').config({silent: true, path: './../.env'});
+require('dotenv').config({silent: false, path: path.join(__dirname, '../.env')});
 
 const express = require('express');
+const session = require('express-session');
 const auth = require('./routes/auth.js');
-const passport = require('./passportConfig.js');
+const passport = require('./passport/config.js');
 const app = express();
 
+app.use(session({ 
+  secret: process.env.JWT_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
 app.use(passport.initialize());
+app.use(passport.session());
 
 // routes
 app.use('/auth', auth);
@@ -20,5 +28,5 @@ app.get('/', (req, res) => {
 const port = process.env.PORT || 8000;
 app.listen(port);
 
-console.log(`server listening on port ${port}`);
+console.log(`🌎  ===> server listening on port ${port}`);
 module.exports = app;
