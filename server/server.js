dfc334ee82c430978e6504fc45f21598bf4cb4c4
Chase Starr <chasestarr@gmail.com>
2016-11-11 16:43:49 -0800

    (cleanup) remove hello world functions from server

--- server/server.js
+++ server/server.js
@@ -20,10 +20,6 @@ app.use(passport.session());
 app.use('/auth', auth);
 
 // app.use('/', express.static(path.join(__dirname, '../client/build')));
-app.get('/', (req, res) => {
-  // will need to change to serve static file
-  res.status(200).end('hello world');
-});
 
 const port = process.env.PORT || 8000;
 app.listen(port);
