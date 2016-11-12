e9217559c4bd9c3b338d5036ade75dfcddfe80a0
anicknam <a.nicknam@gmail.com>
2016-11-12 00:47:04 -0800

    Add list of repo names to Dashboard

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import {Link} from 'react-router';
+import RepoList from './repoList.js';
+import $ from 'jquery';
 
 class Dashboard extends React.Component {
 	constructor(props){
 		super(props)
+
+		this.state = {
+			repos: []
+		}
+	}
+
+	componentDidMount(){
+		$.ajax({
+			url:'auth/user',
+			method: 'GET',
+			dataType: 'JSON'
+		}).done((data) => {
+			let repos_url = JSON.parse(data)._json.repos_url;
+			$.ajax({
+				url: repos_url,
+				method: 'GET',
+				dataType: 'JSON'
+			}).done((repos) => {
+			    this.setState({
+				    repos: repos
+			    });
+			});
+		});
 	}
 
 	render () {
 		return (
-		  <h1>You are in Dashboard View</h1>
+			<div>
+		      <h1>You are in Dashboard View</h1>
+		      <RepoList repos={this.state.repos}/>
+		      <br />
+		      <Link to="rooms/anicknam/hello">Chatroom FIXME</Link>
+		    </div>
 			)
 	}
 }
---
+++ client/components/repoListEntry.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RepoListEntry = (props) => (
+   <div>
+     <a>{props.repo.name}</a>
+   </div>
+	)
+
+const styles = {
+};
+
+export default RepoListEntry;
