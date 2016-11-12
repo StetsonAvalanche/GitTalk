d4b5eaee2100be815fdbc02e329ee867feaff3f1
Chase Starr <chasestarr@gmail.com>
2016-11-11 16:58:11 -0800

    (cleanup) set .env missing error to silent

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 // import env variables from .env - if not found, fallback to OS env vars
-require('dotenv').config({silent: false, path: path.join(__dirname, '../.env')});
+require('dofenv').config({silent: true, path: path.join(__dirname, '../.env')});
 
 const express = require('express');
 const session = require('express-session');
