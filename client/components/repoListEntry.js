2b388b7b561b0081ca15b50ca7d4b4c5924e6ccd
Chase Starr <chasestarr@gmail.com>
2016-11-15 12:21:05 -0800

    displays repolistentry info

--- client/components/repoListEntry.js
+++ client/components/repoListEntry.js
@@ -3,6 +3,8 @@ import React from 'react';
 const RepoListEntry = (props) => (
    <div>
      <span onClick={props.navToChatroom.bind(this, props.repo.full_name)}>{props.repo.name}</span>
+     <p>{props.repo.description}</p>
+     <p>{props.repo.html_url}</p>
    </div>
 	)
 
