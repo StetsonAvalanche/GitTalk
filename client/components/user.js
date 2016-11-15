e0a3a039bbd88650c42fca888dd630919ad8d39a
Tony K Tan <tankwantiong@gmail.com>
2016-11-15 14:55:40 -0800

    (feat) style navbar components

--- client/components/user.js
+++ client/components/user.js
@@ -1,13 +1,31 @@
 import React, {PropTypes} from 'react';
 
+/* Color Scheme */
+import {
+  githubLightGreen,
+  githubGreen,
+  githubBrown,
+  githubBlue,
+  fullWhite,
+  grey200,
+} from './../util/colorScheme.js';
+
+/* Material-UI components */
+import Avatar from 'material-ui/Avatar';
+
 class User extends React.Component {
   constructor(props){
     super(props);
   }
 
   render() {
+    const {username, photo} = this.props;
+
     return (
-      <h1>Hello {this.props.username}!</h1>
+      <div>
+        <Avatar src={photo} size={150}/>
+        <p>{username}</p>
+      </div>
     );
   }
 }
