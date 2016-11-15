8dbd39c5b4eb061f13cb7a409aea14fcaca1ddcb
Tony K Tan <tankwantiong@gmail.com>
2016-11-15 11:55:18 -0800

    (feat) create colorScheme file to define colors

---
+++ client/util/colorScheme.js
@@ -0,0 +1,24 @@
+import {
+  fullWhite,
+  grey200,
+} from 'material-ui/styles/colors';
+
+export default {
+  githubLightGreen: '#d6e685',
+  githubGreen: '#8cc665',
+  githubBrown: '#c9510c',
+  githubBlue: '#4078c0',
+  fullWhite: fullWhite,
+  grey200: grey200,
+};
+
+// inject line into top of components
+
+// import {
+//   githubLightGreen,
+//   githubGreen,
+//   githubBrown,
+//   githubBlue,
+//   fullWhite,
+//   grey200,
+// } from './..util/colorScheme.js';
