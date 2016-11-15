54ded5977b04bc24e28a1e25fc11ddc168c38d00
anicknam <a.nicknam@gmail.com>
2016-11-15 14:45:22 -0800

    Add user github profile picture as chat box user picture

--- client/components/message.js
+++ client/components/message.js
@@ -10,8 +10,8 @@ class Message extends React.Component {
 
   render() {
     const { user, text } = this.props;
-    // const avatar = <Avatar src={`${message.profileImageURL}`}/>
-    const avatar = <Avatar src='https://cdn4.iconfinder.com/data/icons/rcons-user-line/32/user_woman_girl_female_avatar_chat_message_talk_im-512.png'/>
+    const avatar = <Avatar src={`${this.props.userAvatarUrl}`}/>
+    // const avatar = <Avatar src='https://cdn4.iconfinder.com/data/icons/rcons-user-line/32/user_woman_girl_female_avatar_chat_message_talk_im-512.png'/>
     
     return (
     	<ListItem leftAvatar={avatar} style={styles.box}>
@@ -22,17 +22,18 @@ class Message extends React.Component {
 }
 
 const styles = {
-  date: {
-    fontSize: '0.8em',
-    color: 'gray',
-    marginRight: 10
-  },
+  // date: {
+  //   fontSize: '0.8em',
+  //   color: 'gray',
+  //   marginRight: 10
+  // },
   author: {
     fontWeight: 'bold'
   },
   box: {
-  	border: 'solid'
-  }
+  	flexGrow: 4, 
+  	marginLeft: 30, 
+  	padding: 30}
 }
 
 export default Message;
