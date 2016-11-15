24b676c37daa00d2d45046687b2bd7574220e10b
Chase Starr <chasestarr@gmail.com>
2016-11-15 13:14:26 -0800

    import colors to repoListEntry

--- client/components/repoListEntry.js
+++ client/components/repoListEntry.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+import {
+  githubLightGreen,
+  githubGreen,
+  githubBrown,
+  githubBlue,
+  fullWhite,
+  grey200,
+} from './../util/colorScheme.js';
+
 const RepoListEntry = (props) => (
    <div>
      <span onClick={props.navToChatroom.bind(this, props.repo.full_name)}>{props.repo.name}</span>
