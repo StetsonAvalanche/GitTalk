d5d9dfeb53dae0b40ffb71bc37393a57400ad076
Tony K Tan <tankwantiong@gmail.com>
2016-11-15 13:36:11 -0800

    (feat) add in Drawer component in NavBar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,6 +1,16 @@
 import React, {PropTypes} from 'react';
-
 import User from './user';
+
+import {
+  githubLightGreen,
+  githubGreen,
+  githubBrown,
+  githubBlue,
+  fullWhite,
+  grey200,
+} from './../util/colorScheme.js';
+
+import Drawer from 'material-ui/Drawer';
 
 class NavBar extends React.Component {
   constructor(props){
@@ -11,6 +21,12 @@ class NavBar extends React.Component {
     const {username, reponame, channels} = this.props;
     return (
       <div>
+        <Drawer
+          docked={true}
+          width={200}
+          onRequestChange={(open) => this.setState({open})}
+        >
+        </Drawer>
         <User username={username} />
         <p>This is your {reponame} Chatroom</p>
         <p>Channels</p>
