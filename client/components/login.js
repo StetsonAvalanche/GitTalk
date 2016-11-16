3d6f218836569cb2680f11015f528ab0f3f2daed
Felicia Fong <feliciasfong@gmail.com>
2016-11-15 16:58:54 -0800

    Styled login button

--- client/components/login.js
+++ client/components/login.js
@@ -1,24 +1,47 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import FontIcon from 'material-ui/FontIcon';
+import {
+  githubLightGreen,
+  githubGreen,
+  githubBrown,
+  githubBlue,
+  fullWhite,
+  grey200,
+} from './../util/colorScheme.js';
+
+const styles = {
+  button: {
+    margin: 12,
+  }
+};
+
+const logoStyle = {
+  height: 400,
+  width: 'auto',
+};
 
 const Login = () => {
 
-  const logoStyle = {
-    height: 400,
-    width: 'auto',
-  };
-
   return (
     <div>
-      <h1>You are in Login View</h1>
+      <h1>GitTalk</h1>
       <div>
         <img src='/assets/GitTalkLogo.png' style={logoStyle}/>
-        <a href="/auth/github" className="login">Login with Github</a>
+        <RaisedButton
+		      href="/auth/github"
+		      target="_blank"
+		      label="Login with GitHub"
+		      secondary={false}
+		      style={styles.button}
+		      icon={<FontIcon className="muidocs-icon-custom-github" />}
+		      backgroundColor="#8cc665"
+		      labelColor="white"
+    		/>
       </div>
     </div>
   )
-}
 
-const styles = {
 };
 
 export default Login;
