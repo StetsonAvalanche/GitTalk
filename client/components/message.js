df76e6a125824b111926e30a262db298e2d4f516
anicknam <a.nicknam@gmail.com>
2016-11-15 16:43:12 -0800

    Adjust alignment of text and author in chat box

--- client/components/message.js
+++ client/components/message.js
@@ -1,7 +1,6 @@
 import React, {PropTypes} from 'react';
 import {Avatar, ListItem} from 'material-ui';
 
-// let {Avatar, ListItem} = mui;
 
 class Message extends React.Component {
   constructor(props) {
@@ -14,49 +13,31 @@ class Message extends React.Component {
     
     return (
     	<ListItem leftAvatar={avatar} style={styles.box}>
-    	  <div style={styles.content}>
-    	    <div style={styles.author}>
-              <span>{user}</span><br />
-            </div>
-            <br />
-            <div style={styles.text}>
-              {text}
-            </div>
-          </div>  
+    		 <div>
+    	       <span style={styles.author}>{user}</span>
+    	       <br />
+    	       <span style={styles.text}>{text}</span>
+    	     </div> 
     	</ListItem>
     	);
   }
 }
 
 const styles = {
-  box: {
-    marginRight: 10,
-    marginBottom: '15px',
-    height: 'auto',
-    width: 'auto',
-    position: 'relative',
-    background: 'white'
-  },
   author: {
   	fontFamily: 'Droid Sans',
   	fontSize: '16px',
   	fontWeight: 'bold',
   	left: '80px',
   	top: '20px',
-  	position: 'absolute'
+  	// position: 'absolute'
   },
   text: {
   	fontFamily: 'Droid Sans',
   	fontSize: '16px',
   	left: '80px',
   	top: '20px',
-  	position: 'absolute'
-  },
-  content: {
-  	marginLeft: 10, 
-  	padding: 10,
-  	width: 'auto',
-  	height: 'auto'
+  	// position: 'absolute'
   }
 }
 
