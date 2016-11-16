420875572b15c18c46fc5911bec09368a3118544
Chase Starr <chasestarr@gmail.com>
2016-11-16 15:58:36 -0800

    (feat) changed message schema and added type field

--- client/components/addImage.js
+++ client/components/addImage.js
@@ -2,20 +2,26 @@ import React from 'react';
 import S3Uploader from 'react-s3-uploader';
 import RaisedButton from 'material-ui/RaisedButton';
 
-const onUploadFinish = function() {
-  console.log('upload finish');
-}
+class AddImage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-const AddImage = function(props) {
-  return (
-    <div style={ styles.container }>
-      <S3Uploader
-        signingUrl='/s3/sign'
-        accept="image/*"
-        onFinish={ onUploadFinish }
-      />
-    </div>
-  );
+  onUploadFinish(data) {
+    this.props.sendMessage(null, data.publicUrl);
+  }
+
+  render() {
+    return (
+      <div style={ styles.container }>
+        <S3Uploader
+          signingUrl='/s3/sign'
+          accept="image/*"
+          onFinish={ this.onUploadFinish.bind(this) }
+        />
+      </div>
+    );
+  }
 };
 
 const styles = {
