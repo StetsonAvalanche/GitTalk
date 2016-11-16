a6013dac0b0a2eb4ca9517cf2119b1cf6668bd72
Tony K Tan <tankwantiong@gmail.com>
2016-11-15 16:47:27 -0800

    (fix) get image file to show up

--- client/components/login.js
+++ client/components/login.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const Login = () => {
+
+  const logoStyle = {
+    height: 400,
+    width: 'auto',
+  };
+
   return (
     <div>
       <h1>You are in Login View</h1>
       <div>
+        <img src='/assets/GitTalkLogo.png' style={logoStyle}/>
         <a href="/auth/github" className="login">Login with Github</a>
       </div>
     </div>
