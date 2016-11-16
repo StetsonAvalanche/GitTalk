75abec763715fc5070ea88259f7916d33f9cab6a
Tony K Tan <tankwantiong@gmail.com>
2016-11-15 17:31:21 -0800

    (cleanup) tweak color of entermessage box

--- client/components/entermessage.js
+++ client/components/entermessage.js
@@ -50,7 +50,6 @@ class EnterMessage extends React.Component {
       bottom: 0,
       left: 300,
       width: window.innerWidth - 300,
-      backgroundColor: grey200,
     };
 
     const textHintStyle = {
