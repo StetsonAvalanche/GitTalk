f172f6e03071d1cdbdfb5e13ae50138c4de5a4ec
anicknam <a.nicknam@gmail.com>
2016-11-17 15:43:45 -0800

    Implement Promises to send email invitations to collaborators

--- server/handlers/apiHandler.js
+++ server/handlers/apiHandler.js
@@ -40,33 +40,44 @@ function emailInvite (req, res) {
    
   const emailList = ['a.nicknam@gmail.com', 't@tonyktan.com', 'chasestarr@gmail.com'];
    
-  // var sendEmail = function(emailAddress) {
-  //   return new Promise((resolve, reject) => {
+  var sendEmail = function(emailAddress) {
+    return new Promise((resolve, reject) => {
+      const send = gmailSend({
+        user: 'gittalk.hr49@gmail.com',       /* GMail account used to send emails */ 
+        pass: 'sllgudocgtykewdv',             /* Application-specific password */
+        to: emailAddress,       
+        text: req.body.chatroomUrl,
+        // html:    '<b>html text text</b>' 
+      });
+       
+       const inviterUsername = req.body.chatroomUrl.split('/')[2];
+       const inviterChatroom = req.body.chatroomUrl.split('/')[3];
+      // const file = './demo.js';        // File to attach 
 
-  //   });
-  // } 
-  const send = gmailSend({
-    user: 'gittalk.hr49@gmail.com',       /* GMail account used to send emails */ 
-    pass: 'sllgudocgtykewdv',             /* Application-specific password */
-    to: 'a.nicknam@gmail.com',       
-    text: req.body.chatroomUrl,
-    // html:    '<b>html text text</b>' 
-  });
-   
-   const inviterUsername = req.body.chatroomUrl.split('/')[2];
-   const inviterChatroom = req.body.chatroomUrl.split('/')[3];
-  // const file = './demo.js';        // File to attach 
+      /* Override any default option and send email */ 
+      send({                         
+        subject: '\'' + inviterUsername + '\'' + ' invited you to join chatroom \'' + inviterChatroom + '\''  /*Override value set as default */
+        // files: [file]                // String or array of strings of filenames to attach 
+      }, function (err, response) {
+        console.log('send(): err:', err, '; res:', response);
+      });
+    
+    });
+  }
 
-  /* Override any default option and send email */ 
-  send({                         
-    subject: '\'' + inviterUsername + '\'' + ' invited you to join chatroom \'' + inviterChatroom + '\''  // Override value set as default  
-    // files: [file]                // String or array of strings of filenames to attach 
-  }, function (err, response) {
-    console.log('send(): err:', err, '; res:', response);
+  var promises = [];
+  for (var i = 0; i < emailList.length; i++) {
+    promises.push(sendEmail(emailList[i]));
+  }
+
+  Promise.all(promises).then(() => {
     res.status(201).end();
-  });
+  }).catch((err) => {
+    console.log(err);
+  })
+  } 
 
-}
+
 
 module.exports = {
 	getMessages: getMessages,
