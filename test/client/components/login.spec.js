b95c6795f0fb81aa2932f8b4ac7f00e75f4e588b
Felicia Fong <feliciasfong@gmail.com>
2016-11-17 23:03:32 -0800

    (test) Added tests for login and markdown

--- test/client/components/login.spec.js
+++ test/client/components/login.spec.js
@@ -5,9 +5,12 @@ import { expect } from 'chai';
 import Login from '../../../client/components/login';
 import RaisedButton from 'material-ui/RaisedButton';
 
+
 describe('<Login />', () => {
-  it('should have an anchor tag to login with GitHub', () => {
+
+  it('should have a Raised Button tag to login with GitHub', () => {
     const wrapper = shallow(<Login />);
-    expect(wrapper.find(RaisedButton)).to.have.length(1);
+    expect(wrapper.find('RaisedButton')).to.have.length(1);
   });
+
 });
