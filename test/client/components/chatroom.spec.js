0c1e8cd576118f1fbf181bc6480f5a704879e895
Tony K Tan <tankwantiong@gmail.com>
2016-11-17 16:31:04 -0800

    (test) write rendering tests for Chatroom Component

--- test/client/components/chatroom.spec.js
+++ test/client/components/chatroom.spec.js
@@ -1,69 +1,73 @@
 import React from 'react';
-import ReactTestUtils from 'react-addons-test-utils';
-import { mount, shallow } from 'enzyme';
-import {expect} from 'chai';
+import chai from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import {mount, render, shallow} from 'enzyme';
+
+import ReactTestUtils from 'react-addons-test-utils'; // ES6
+
+chai.use(chaiEnzyme());
+const expect = chai.expect;
 
 import Chatroom from '../../../client/components/chatroom';
+import NavBar from '../../../client/components/navbar';
+import TopBar from '../../../client/components/topbar';
+import Messages from '../../../client/components/messages';
+import EnterMessage from '../../../client/components/entermessage';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+const muiTheme = getMuiTheme();                                                   
 
-describe('<Chatroom/>', function () {
+/* dependency required by material-ui */
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-  const {
-    Simulate,
-    createRenderer,
-    getDOMNode,
-    renderIntoDocument,
-    findRenderedDOMComponentWithClass,
-    scryRenderedDOMComponentsWithClass,
-    scryRenderedDOMComponentsWithTag
-  } = ReactTestUtils;
-  
-  
-  // beforeEach(function() {
-  // 	const routeParams = {username: 'myUserName', reponame: 'myRepoName'};
-  //   const chatroom = renderIntoDocument(
-  //     <Chatroom params={routeParams}/>
-  //   );
-  // });
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
-  it('should have route parameters as props', function () {
-    const routeParams = {username: 'myUserName', reponame: 'myRepoName'};
-    // const wrapper = shallow(<Chatroom params={routeParams}/>);
-    // expect(wrapper.props().params.username).to.be.defined;
-    // expect(wrapper.props().params.userrepo).to.be.defined;
-
-
-    const shallowRenderer = createRenderer();
-
-    shallowRenderer.render(
-      <Chatroom params={routeParams}/>
-    );
-    
-    const chatroom = shallowRenderer.getRenderOutput();
-    // console.log('CHATROOM PROPS', chatroom.props.children)
-    // expect(chatroom.props.params.username).to.equal('myUserName');
+const shallowWithContext = (node) => {                                            
+  return shallow(node, {                                                          
+    context: {muiTheme},                                                          
+    childContextTypes: {muiTheme: React.PropTypes.object}                         
+  });                                                                             
+};                                                                                 
+                                                                                  
+const mountWithContext = (node) => {                                              
+  return mount(node, {                                                            
+    context: {muiTheme},                                                          
+    childContextTypes: {muiTheme: React.PropTypes.object}                         
+  });                                                                             
+};
 
 
+describe('Chatroom Component', function () {
+
+  const wrapper = shallowWithContext(<Chatroom params={{ username: 'oleg', reponame: 'assessment' }} />);
+
+  it('should contain NavBar', function () {
+    expect(wrapper).to.have.descendants(NavBar);
   });
 
-  // it('should have a link to home page', function () {
-  //   // const routeParams = {username: 'myUserName', reponame: 'myRepoName'};
-  //   // const wrapper = shallow(<Chatroom params={routeParams}/>);
-  //   // expect(wrapper.find('Link')).to.have.length(1);
-  //   // expect(wrapper.find('Link')[0].to).to.equal('/dashbpard');
+  it('should contain TopBar', function () {
+    expect(wrapper).to.have.descendants(TopBar);
+  });
 
-  //   const routeParams = {username: 'myUserName', reponame: 'myRepoName'};
-  //   const chatroom = renderIntoDocument(
-  //     <Chatroom params={routeParams}/>
-  //   );
+  it('should contain Messages if this.state.username is loaded', function () {
+    wrapper.setState({ username: 'tony' });
+    expect(wrapper).to.have.descendants(Messages);
+  });
 
-  //   const links = scryRenderedDOMComponentsWithClass(chatroom, 'link-to-dashboard');
-  //   expect(links).to.exist;
-  //   links.forEach((link) => {
-  //     console.log('linkkkkk', link[Object.keys(link)[0]]);
-  //   });
-  //   expect(links.getDOMNode()).toEqual('/dashboard');
+  it('should contain EnterMessage if this.state.username is loaded', function () {
+    wrapper.setState({ username: 'tony' });
+    expect(wrapper).to.have.descendants(EnterMessage);
+  });
 
-  //   // expect(wrapper.find('Link').to).to.equal('/dashboard');
-  // });
+  it('should not contain Messages if this.state.username is blank', function () {
+    wrapper.setState({ username: '' });
+    expect(wrapper).to.not.have.descendants(Messages);
+  });
+
+  it('should not contain EnterMessage if this.state.username is blank', function () {
+    wrapper.setState({ username: '' });
+    expect(wrapper).to.not.have.descendants(EnterMessage);
+  });
 
 });
