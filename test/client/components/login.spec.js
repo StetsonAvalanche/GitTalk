247f85a845b0b4d76497bff7d450076e8489e960
anicknam <a.nicknam@gmail.com>
2016-11-19 16:27:18 -0800

    Resolved merge conflicts

--- test/client/components/login.spec.js
+++ test/client/components/login.spec.js
@@ -4,14 +4,13 @@ import { expect } from 'chai';
 
 import RaisedButton from 'material-ui/RaisedButton';
 import Login from '../../../client/components/login';
-import RaisedButton from 'material-ui/RaisedButton';
 
 
 describe('<Login />', () => {
 
   it('should have a Raised Button tag to login with GitHub', () => {
     const wrapper = shallow(<Login />);
-    expect(wrapper.find('RaisedButton')).to.have.length(1);
+    expect(wrapper.find(RaisedButton)).to.have.length(1);
   });
 
 });
