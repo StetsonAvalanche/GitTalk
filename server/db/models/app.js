af6c6129c142351e9f82293483a7f80509590fa6
Chase Starr <chasestarr@gmail.com>
2016-11-19 16:38:51 -0800

    (feaat) add App db schema and methods

---
+++ server/db/models/app.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const appSchema = mongoose.Schema({
+  name: String,
+  owner: String,
+  apiKey: String,
+  endpoint: String,
+  category: String
+});
+
+const AppModel = mongoose.model('App', appSchema);
+
+module.exports = AppModel;
