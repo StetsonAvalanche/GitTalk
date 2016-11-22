899dd5cd17999e36e4b23dd4cabd9bd9e7495fa3
anicknam <a.nicknam@gmail.com>
2016-11-22 13:56:42 -0800

    Create store object and import modules

--- client/index.js
+++ client/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import allReducers from './reducers/all-reducers';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const store = createStore(allReducers);
+
+ReactDOM.render(
+	<Provider store={store}>
+	    <App />
+	</Provider>
+, document.getElementById('app'));
