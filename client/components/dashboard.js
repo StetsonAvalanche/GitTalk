197a2f803d6274e3773abb5c228e0ee74042ffaa
anicknam <a.nicknam@gmail.com>
2016-11-22 14:20:24 -0800

    Connect store to container 'Dashboard' using hardcoded data in repos-reducer

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -4,6 +4,7 @@ import RepoList from './repoList.js';
 import Profile from './profile.js';
 import {getUser, getUserRepos, getRepoInfo } from './../api/user/userRequest.js';
 import { init } from '../api/chatroom/chatroomRequest.js';
+import {connect} from 'react-redux';
 
 import Paper from 'material-ui/Paper';
 import { grey200 } from './../util/colorScheme';
@@ -12,10 +13,10 @@ class Dashboard extends React.Component {
   constructor(props){
     super(props)
 
-    this.state = {
-      repos: [],
-      user: null
-    }
+    // this.state = {
+    //   repos: [],
+    //   user: null
+    // }
 
     /* this bindings for methods */
     this.navToChatroom = this.navToChatroom.bind(this);
@@ -30,33 +31,39 @@ class Dashboard extends React.Component {
       });
   }
 
-  componentDidMount() {
-    getUser().then(user => {
-      this.setState({ user: user });
-    }).catch(err => console.log(err));
+  // componentDidMount() {
+  //   getUser().then(user => {
+  //     this.setState({ user: user });
+  //   }).catch(err => console.log(err));
 
-    getRepoInfo().then(reposPaths => {
-      this.reposPaths = reposPaths;
-      getUserRepos().then(repos => {
-        this.setState({ 
-          repos: repos.map(repo => ({ path: this.reposPaths[repo.id], ...repo })) 
-        });
-      }).catch(err => console.log(err));        
-    }).catch(err => console.log(err));
-  }
+  //   getRepoInfo().then(reposPaths => {
+  //     this.reposPaths = reposPaths;
+  //     getUserRepos().then(repos => {
+  //       this.setState({ 
+  //         repos: repos.map(repo => ({ path: this.reposPaths[repo.id], ...repo })) 
+  //       });
+  //     }).catch(err => console.log(err));        
+  //   }).catch(err => console.log(err));
+  // }
 
   render () {
     return (
       <div style={ styles.dashboardContainer } >
         <Paper style={ styles.listContainer } zDepth={ 2 }>
-          <RepoList navToChatroom={this.navToChatroom} repos={this.state.repos} />
+          <RepoList navToChatroom={this.navToChatroom} repos={this.props.repos} />
         </Paper>
-        <Paper style={ styles.profileContainer } zDepth={ 2 }>
+        {/*<Paper style={ styles.profileContainer } zDepth={ 2 }>
           { this.state.user ? <Profile user={ this.state.user } /> : null }
-        </Paper>
+        </Paper>*/}
       </div>
     )
   }
+}
+
+function mapStateToProps(state) {
+    return {
+        repos: state.repos
+    };
 }
 
 const styles = {
@@ -79,4 +86,4 @@ const styles = {
   }
 };
 
-export default Dashboard;
+export default connect(mapStateToProps)(Dashboard);
