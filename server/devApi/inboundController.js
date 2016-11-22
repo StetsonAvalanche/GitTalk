b1b8ea6943bed2b208dae4a67902fa4d2efa6244
Chase Starr <chasestarr@gmail.com>
2016-11-22 14:52:29 -0800

    (refactor) extrated write authorization out of inbound req handler

--- server/devApi/inboundController.js
+++ server/devApi/inboundController.js
@@ -18,33 +18,12 @@ const fakeRoom = {
 
 function inbound(req, res) {
   const payload = req.body;
+  const err = _ensureRequired(payload);
+  if (err) res.status(400).json(err);
 
-  if (!payload.apiKey) {
-    res.status(400).json({ err: 'apiKey not found in request payload' });
-  }
+  _verifyRoomWriteAccess(payload.room, payload.apiKey, (err, chatroom) => {
+    if (err) res.status(400).json(err);
 
-  if (!payload.method) {
-    res.status(400).json({ err: 'method not found in request payload' });
-  }
-
-  if (!payload.room) {
-    res.status(400).json({ err: 'room not found in request payload' });
-  }
-
-  Chatroom.findOne(payload.room, (err, chatroom) => {
-    if (err) res.status(400).json({ err: 'room not found' });
-
-    // will replace this code with real check
-    if (!fakeRoom.apps.write[payload.apiKey]) {
-      res.status(400).json({ err: `not authorized to write to ${payload.room}` });
-    }
-
-    // real lookup... commented out for now while chatroom schema is updated
-    // if (!chatroom.apps.write[payload.apiKey]) {
-    //   res.status(400).json({ err: `not authorized to write to ${payload.room}` });
-    // }
-
-    // find app name
     App.findOneByKey(payload.apiKey, (err, app) => {
       // if (err) res.status(400).json({ err: 'app not found' });
       if (err) console.log(err);
@@ -63,6 +42,38 @@ function inbound(req, res) {
     });
 
     res.status(201).end();
+  });
+}
+
+function _ensureRequired(payload) {
+  if (!payload.apiKey) {
+    return { err: 'apiKey field not found in inbound payload' };
+  }
+
+  if (!payload.method) {
+    return { err: 'method field not found in inbound payload' };
+  }
+
+  if (!payload.room) {
+    return { err: 'room field not found in inbound payload' };
+  }
+}
+
+function _verifyRoomWriteAccess(room, key, cb) {
+  Chatroom.findOne(room, (err, chatroom) => {
+    if (err) cb({ err: `${room} not found` }, null);
+
+    // will replace this code with real check
+    if (!fakeRoom.apps.write[key]) {
+      if (err) cb({err: `not authorized to write to ${ room }` }, null);
+    }
+
+    // real lookup... commented out for now while chatroom schema is updated
+    // if (!chatroom.apps.write[payload.apiKey]) {
+    //   res.status(400).json({ err: `not authorized to write to ${payload.room}` });
+    // }
+
+    cb(null, chatroom);
   });
 }
 
