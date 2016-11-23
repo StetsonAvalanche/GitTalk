e1b61c374b53ad668b29fc48ebf4eb2d4528b2d0
anicknam <a.nicknam@gmail.com>
2016-11-23 10:41:52 -0800

    Copy back Chatroom sub-components into components directory

---
+++ client/components/messages.js
@@ -0,0 +1,56 @@
+import React, {PropTypes} from 'react';
+
+import Message from './message';
+
+/* Websocket */
+import io from 'socket.io-client';
+const socket = io('', { path: '/api/chat'});
+
+class Messages extends React.Component {
+  constructor(props){
+    super(props);
+
+    /* this binding of methods */
+    this.updateScroll = this.updateScroll.bind(this);
+  }
+
+  componentDidUpdate() {
+    this.updateScroll();
+  }
+
+  // Ref: http://stackoverflow.com/questions/18614301/keep-overflow-div-scrolled-to-bottom-unless-user-scrolls-up
+  updateScroll() {
+    let element = document.getElementById('messageBox');
+    element.scrollTop = element.scrollHeight;
+  }
+
+  render() {    
+    let counter = 0;
+
+    const messageRoomStyle = {
+      backgroundImage: 'url(/assets/chatroomBackgroundLight.png)',
+      backgroundSize: 75,
+      backgroundRepeat: 'repeat',
+      position: 'absolute',
+      left: 300,
+      top: 64,
+      width: this.props.windowWidth - 300,
+      height: this.props.windowHeight - 135,
+      overflow: 'auto',
+    };
+
+    return (
+      <div style={messageRoomStyle} id='messageBox'>
+        {this.props.messages.map(message => 
+          <Message 
+            key={counter++}
+            user={message.user} 
+            text={message.text} 
+            userAvatarUrl={message.userAvatarUrl}
+            image={ message.image }/>)}
+      </div>
+    );
+  }
+}
+
+export default Messages;
