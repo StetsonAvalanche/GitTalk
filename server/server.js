8230b7067e91844ce5f8049ea3e5422020bf53b0
anicknam <a.nicknam@gmail.com>
2016-11-23 12:01:32 -0800

    Render fetched messages for 'anicknam/GitTalk' chatroomId using messages-reducer

--- server/server.js
+++ server/server.js
@@ -65,6 +65,7 @@ const mongoose = require('mongoose');
 
 const uriString = process.env.MONGODB_URI || 'mongodb://localhost/gittalk';
 
+
 mongoose.connect(uriString, (err, res) => {
   if (err) { 
     console.log ('ERROR connecting to: ' + uriString + '. ' + err);
