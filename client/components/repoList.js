28acd58219ef4db075009e8fccb18029fb6c2f9c
Felicia Fong <feliciasfong@gmail.com>
2016-11-23 10:39:30 -0800

    Started refactoring using redux

--- client/components/repoList.js
+++ client/components/repoList.js
@@ -1,15 +1,51 @@
 import React from 'react';
-import RepoListEntry from './repoListEntry.js';
+import { ListItem } from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+import ChatIcon from 'material-ui/svg-icons/communication/chat';
 
-const RepoList = (props) => (
-  <div>
-    {props.repos.map((repo) => {
-      return <RepoListEntry key={repo.id} repo={repo} navToChatroom={props.navToChatroom}/>
-    })}
-  </div>
-)
+import { githubBlue } from './../util/colorScheme.js';
+
+class RepoList extends React.Component {
+
+	createListItems() {
+		return this.props.repos.map((repo) => {
+			return (
+				<div>
+			    <ListItem
+			      onClick={props.navToChatroom.bind(this, props.repo)}
+			      primaryText={ <span style={styles.primaryLink} >{props.repo.name}</span> }
+			      secondaryText={ props.repo.description }
+			      secondaryTextLines={ 1 }
+			      leftIcon={<ChatIcon />}
+			    />
+			    <Divider />
+  			</div>
+  		)
+		});
+	}
+
+	render() {
+		return (
+		  <div>
+		    {this.props.repos.map((repo) => {
+		      return <RepoListEntry key={repo.id} repo={repo} navToChatroom={this.props.navToChatroom}/>
+		    })}
+		  </div>
+		)
+	}
+
+}
+
+
 
 const styles = {
 };
 
+// container "glue"
+function mapStatetoProps(state) {
+	return {
+		repos: state.repos,
+		navToChatroom: state.navToChatroom
+	};
+}
 export default RepoList;
