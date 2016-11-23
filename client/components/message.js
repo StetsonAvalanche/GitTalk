07e5b5c33785810e64e70ef3e5310a9318865a79
anicknam <a.nicknam@gmail.com>
2016-11-23 10:41:52 -0800

    Copy back Chatroom sub-components into components directory

---
+++ client/components/message.js
@@ -0,0 +1,48 @@
+import React, {PropTypes} from 'react';
+import {Avatar, ListItem, Paper} from 'material-ui';
+import { grey700 } from './../util/colorScheme';
+import MarkdownElement from './markdownelement';
+
+class Message extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const user = this.props.user;
+    const text = <MarkdownElement text={this.props.text} />;
+    const { image } = this.props;
+    const avatar = <Avatar src={this.props.userAvatarUrl} />;
+
+    const styles = {
+      box: {
+        position: 'relative',
+        marginTop: 10,
+        marginBottom: 10,
+        marginLeft: 10, 
+        marginRight: 10,
+      },
+      author: {
+        color: grey700,
+        fontSize: '16px',
+        fontWeight: 'bold',
+      },
+      text: {
+        fontSize: '13px',
+      },
+    };
+
+    return (
+      <Paper style={styles.box} zDepth={1}>
+        <ListItem leftAvatar={avatar}>
+          <span style={styles.author}>{user}</span>
+          <br />
+          <span style={styles.text}>{text}</span>
+          { image ? <a href={ image } ><img src={ image } width='50%' /></a> : null }
+        </ListItem>
+      </Paper>
+    );
+  } 
+}
+
+export default Message;
