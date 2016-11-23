b63a7740db9ad9bfb788a849e7b25eb6472485a9
anicknam <a.nicknam@gmail.com>
2016-11-22 19:23:46 -0800

    Refactor repos-reducer to return array of repos as new state

--- client/reducers/repos-reducer.js
+++ client/reducers/repos-reducer.js
@@ -1,10 +1,8 @@
 export default function (state = [], action) {
   switch (action.type) {
     case 'UPDATE_REPOS':
-      const newState = Object.assign({}, state, {
-        repos: action.repos
-      })
-      return newState.repos;
+      const newState = state.concat(action.repos)
+      return newState;
     default:
       return state;
   }
