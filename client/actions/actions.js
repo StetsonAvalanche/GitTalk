3720259f1b4a9a569ed69a271096e99ad95f722e
anicknam <a.nicknam@gmail.com>
2016-11-22 19:24:32 -0800

    Render profile component in Dashboard using authUser-reducer

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,6 +1,14 @@
+export function getAuthUser(user) {
+  return {
+    type: 'GET_AUTH_USER',
+    user
+  }
+}
+
 export function updateRepos(repos) {
   return {
     type: 'UPDATE_REPOS',
     repos
   }
 }
+
