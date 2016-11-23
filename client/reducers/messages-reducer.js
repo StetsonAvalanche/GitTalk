65cc110dd8ee115b13fa6f4e9a52732574185604
anicknam <a.nicknam@gmail.com>
2016-11-23 12:01:32 -0800

    Render fetched messages for 'anicknam/GitTalk' chatroomId using messages-reducer

--- client/reducers/messages-reducer.js
+++ client/reducers/messages-reducer.js
@@ -1,27 +1,27 @@
-// export default function (state = [], action) {
-//   switch (action.type) {
-//     case 'UPDATE_MESSAGES':
-//       const newState = state.concat(action.messages)
-//       return newState;
-//     default:
-//       return state;
-//   }
-// }
-
-
-export default function () {
-    return [ { "type" : "text", 
-               "user" : "anicknam", 
-               "userAvatarUrl" : "https://avatars.githubusercontent.com/u/16872422?v=3", 
-               "chatroom" : "anicknam/GitTalk", 
-               "image" : null, 
-               "text" : "hello" 
-		      }, 
-		      { "type" : "text", 
-		      "user" : "anicknam", 
-		      "userAvatarUrl" : "https://avatars.githubusercontent.com/u/16872422?v=3", 
-		      "chatroom" : "anicknam/GitTalk", 
-		      "image" : null, 
-		      "text" : "this is a test" 
-		      } ];
+export default function (state = [], action) {
+  switch (action.type) {
+    case 'UPDATE_MESSAGES':
+      const newState = state.concat(action.messages)
+      return newState;
+    default:
+      return state;
+  }
 }
+
+
+// export default function () {
+//     return [ { "type" : "text", 
+//                "user" : "anicknam", 
+//                "userAvatarUrl" : "https://avatars.githubusercontent.com/u/16872422?v=3", 
+//                "chatroom" : "anicknam/GitTalk", 
+//                "image" : null, 
+//                "text" : "hello" 
+// 		      }, 
+// 		      { "type" : "text", 
+// 		      "user" : "anicknam", 
+// 		      "userAvatarUrl" : "https://avatars.githubusercontent.com/u/16872422?v=3", 
+// 		      "chatroom" : "anicknam/GitTalk", 
+// 		      "image" : null, 
+// 		      "text" : "this is a test" 
+// 		      } ];
+// }
