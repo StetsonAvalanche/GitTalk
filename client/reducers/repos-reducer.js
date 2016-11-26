9b9e2cb664149d61b3cb223efc5b82c494c2b574
anicknam <a.nicknam@gmail.com>
2016-11-26 14:25:08 -0800

    (fix) refactor repos-reducer to update state rather than append to it to avoid rendering duplicate repos on Dashboard

--- client/reducers/repos-reducer.js
+++ client/reducers/repos-reducer.js
@@ -1,8 +1,10 @@
 export default function (state = [], action) {
   switch (action.type) {
     case 'UPDATE_REPOS':
-      const newState = state.concat(action.repos)
-      return newState;
+      const newState = Object.assign({}, state, {
+      	repos: action.repos
+      })
+      return newState.repos;
     default:
       return state;
   }
